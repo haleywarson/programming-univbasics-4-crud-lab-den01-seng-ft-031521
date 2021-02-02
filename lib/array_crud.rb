4248e22a5df8378f246a3423911a7048b300a821
Haley Warson <haleywarson@gmail.com>
2021-02-02 17:21:01 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -19,21 +19,21 @@ def remove_element_from_end_of_array(array)
 end
 
 def remove_element_from_start_of_array(array)
-  
+  remove_element_from_start_of_array.shift()
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+  retrieve_element_from_inde[index_number]
 end
 
 def retrieve_first_element_from_array(array)
-  
+  retrieve_first_element_from_array.shift(array)
 end
 
 def retrieve_last_element_from_array(array)
-  
+  retrieve_last_element_from_array.pop(array)
 end
 
 def update_element_from_index(array, index_number, element)
-
+  update_element_from_index
 end
