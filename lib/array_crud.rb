1dcd57735ab308d5381078ee58f883b3a90343d4
Haley Warson <haleywarson@gmail.com>
2021-02-02 17:19:24 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,15 +7,15 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  add_element_to_end_of_array << element
+  add_element_to_end_of_array << ele
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  add_element_to_start_of_array.unshift(ele)
 end
 
 def remove_element_from_end_of_array(array)
-  
+  remove_element_from_end_of_array.pop(ele)
 end
 
 def remove_element_from_start_of_array(array)
